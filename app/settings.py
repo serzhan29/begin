49ab7054580501625ce16695f0859497b92a8114
serzhan29 <133089140+serzhan29@users.noreply.github.com>
2024-03-12 22:51:27 +0500

    last

--- app/settings.py
+++ app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ps=jv(1@d=7hu%igok$i6p@ba(*5$z!ao#i%xri+i3iekljx$u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
